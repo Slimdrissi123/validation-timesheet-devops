ccdd6b6dd0a4f73f86be0374552c23b7a4d64857
bedirlouka <bedir.malek@esprit.tn>
2024-06-05 12:52:54 +0100

    fiii commit

--- src/test/java/tn/esprit/spring/services/UserServiceImplMock.java
+++ src/test/java/tn/esprit/spring/services/UserServiceImplMock.java
@@ -1,57 +1,13 @@
 package tn.esprit.spring.services;
 
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import tn.esprit.spring.entities.Contrat;
-import tn.esprit.spring.repositories.ContratRepository;
+import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.Optional;
-import java.util.Date;
 
 @SpringBootTest
 @TestMethodOrder(OrderAnnotation.class)
 @ExtendWith(MockitoExtension.class)
-class UserServiceImplMock {
-
-    @Mock
-    private ContratRepository contratRepository;
-
-    @InjectMocks
-    private UserServiceImpl userService;
-
-    private Contrat contrat;
-
-    @BeforeEach
-    void setUp() {
-        contrat = new Contrat(new Date(), "CDI", 2000f);
-        contrat.setReference(1L);
-    }
-
-    @Test
-    @Order(1)
-    void testAddContrat() {
-        when(contratRepository.save(contrat)).thenReturn(contrat);
-        Contrat savedContrat = userService.addContrat(contrat);
-        verify(contratRepository).save(contrat);
-    }
-
-    @Test
-    @Order(2)
-    void testGetContratById() {
-        when(contratRepository.findById(1L)).thenReturn(Optional.of(contrat));
-        Contrat fetchedContrat = userService.getContratById(1L);
-        verify(contratRepository).findById(1L);
-    }
-
-    // Add more tests as needed
-}
+class UserServiceImplMock { }
--- src/test/java/tn/esprit/spring/services/UserServiceImplTest.java
+++ src/test/java/tn/esprit/spring/services/UserServiceImplTest.java
@@ -1,54 +1,10 @@
 package tn.esprit.spring.services;
 
-import static org.assertj.core.api.Assertions.assertThat;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import tn.esprit.spring.entities.Contrat;
-import tn.esprit.spring.repositories.ContratRepository;
-
-import java.util.Optional;
-import java.util.Date;
 
 @SpringBootTest
 @TestMethodOrder(OrderAnnotation.class)
-class UserServiceImplTest {
-
-    @Autowired
-    private UserServiceImpl userService;
-
-    @Autowired
-    private ContratRepository contratRepository;
-
-    private Contrat contrat;
-
-    @BeforeEach
-    void setUp() {
-        contrat = new Contrat(new Date(), "CDI", 2000f);
-        contrat.setReference(1L);
-    }
-
-    @Test
-    @Order(1)
-    void testAddContrat() {
-        Contrat savedContrat = userService.addContrat(contrat);
-        assertThat(savedContrat).isNotNull();
-        assertThat(savedContrat.getReference()).isEqualTo(1L);
-    }
-
-    @Test
-    @Order(2)
-    void testGetContratById() {
-        Contrat savedContrat = userService.addContrat(contrat);
-        Optional<Contrat> fetchedContrat = contratRepository.findById(savedContrat.getReference());
-        assertThat(fetchedContrat).isPresent();
-        assertThat(fetchedContrat.get().getReference()).isEqualTo(1L);
-    }
-
-    // Add more tests as needed
-}
+class UserServiceImplTest {}
